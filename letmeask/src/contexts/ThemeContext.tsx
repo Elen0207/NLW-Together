12ffb66e5e26c5ca63edb9ed6ecc31c4bc8ee166
Elen Carvalho <60263754+Elen0207@users.noreply.github.com>
2021-06-23 13:06:45 -0300

    criando uma tema dark ou ligth

---
+++ letmeask/src/contexts/ThemeContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../themes/theme";
+import { GlobalStyles } from "../themes/globals";
+import { Theme } from "../themes/styles";
+import * as themes from "../themes/theme";
+
+type ThemeContextData = {
+  isLight: boolean;
+  theme: Theme | undefined;
+  toggleTheme: () => void;
+};
+
+type ThemeContextProviderProps = {
+  children: ReactNode;
+};
+
+export const ThemeContext = createContext({} as ThemeContextData);
+
+export function ThemeContextProvider({ children }: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState(themes.lightTheme);
+  const isLight = theme === themes.lightTheme;
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+
+    localTheme && setTheme(JSON.parse(localTheme));
+  }, []);
+
+  const toggleTheme = () => {
+    if (isLight) {
+      window.localStorage.setItem("theme", JSON.stringify(themes.darkTheme));
+      setTheme(themes.darkTheme);
+    } else {
+      window.localStorage.setItem("theme", JSON.stringify(themes.lightTheme));
+      setTheme(themes.lightTheme);
+    }
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        isLight,
+        theme,
+        toggleTheme,
+      }}
+    >
+      <ThemeProvider
+        theme={theme === themes.lightTheme ? lightTheme : darkTheme}
+      >
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
