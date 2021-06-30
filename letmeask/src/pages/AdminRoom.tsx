4fc0d0362e2e4c3935d02bf14687603d90148c40
Elen Carvalho <60263754+Elen0207@users.noreply.github.com>
2021-06-29 22:24:39 -0300

    Aula 04 - page AdminRoom, listen likes, close rooms

---
+++ letmeask/src/pages/AdminRoom.tsx
@@ -0,0 +1,90 @@
+import { useHistory, useParams } from 'react-router-dom';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
+import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
+import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+import '../styles/room.scss';
+
+
+type RoomParams = {
+	id: string;
+}
+
+
+
+export function AdminRoom () {
+	const history = useHistory();
+	const params = useParams<RoomParams>(); 
+	const roomId = params.id;
+	const { questions, title } = useRoom(roomId);
+
+
+	async function handleEndRoom(roomId: string) {
+		if (window.confirm('Tem certeza que você deseja encerrar essa sala?')) {
+			await database.ref(`rooms/${roomId}`).update({ 
+				endedAt: new Date(),
+			})
+
+			history.push('/')
+		}
+	}
+
+
+	async function handleDeleteQuestion(questionId : string) {
+		if (window.confirm('Tem certeza que você deseja excluir essa pergunta?')) {
+			await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+		}
+	}
+
+
+  return (
+    <div id="page-room">
+			<header>
+				<div className="content">
+					<img src={logoImg} alt="Logo Letmeask" />
+          <div>
+            <RoomCode code= {roomId} />
+            <Button 
+							isOutlined 
+							onClick={() =>  handleEndRoom(roomId)} 
+						> 
+							Encerrar sala 
+						</Button>
+          </div>
+				</div>
+			</header>
+
+			<main>
+				<div className="room-title">
+					<h1> Sala {title} </h1>
+					{ questions.length > 0 &&
+						<span> {questions.length} pergunta(s) </span>
+					}
+				</div>
+				
+				<div className="question-list">
+					{ questions.map(question => {
+						return ( 
+							<Question 
+								key={question.id}
+								content={question.content} 
+								author={question.author} 
+							>
+								<button
+									type="button"
+									onClick={() => handleDeleteQuestion(question.id)}
+								>
+									<img src={deleteImg} alt="Delete o like" />
+								</button>
+							</Question>
+						)
+					})}
+				</div>
+			</main>
+		</div>
+  )
+}
--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -40,6 +40,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Está sala já foi encerrada')
+      return;
+    }
+
     history.push(`rooms/${roomCode}`);
   }
   
